775ee44fc9567ae862a4c5cd57d1cd04c014521a
saturn-ss <shopify.saturn@outlook.com>
2025-04-23 12:29:25 -0800

    add shop page

---
+++ app/routes/show.jsx
@@ -0,0 +1,11 @@
+export default function ShopPage() {
+  reutrn (
+    <div>
+      <h1>Shop Page</h1>
+      <p>Welcome to the shop!</p>
+      <p>Here you can find a variety of products to purchase.</p>
+      <p>Feel free to browse through our collection and add items to your cart.</p>
+      <p>Happy shopping!</p>
+    </div>
+  );
+}
