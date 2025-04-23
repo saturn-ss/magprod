20299aeb430d22ec857e2f50e8afc2c109a0ced5
saturn-ss <shopify.saturn@outlook.com>
2025-04-23 12:45:01 -0800

    default shop page file

--- app/routes/shop.jsx
+++ app/routes/shop.jsx
@@ -1,11 +1,15 @@
-export default function ShopPage() {
-  reutrn (
-    <div>
-      <h1>Shop Page</h1>
-      <p>Welcome to the shop!</p>
-      <p>Here you can find a variety of products to purchase.</p>
-      <p>Feel free to browse through our collection and add items to your cart.</p>
-      <p>Happy shopping!</p>
-    </div>
-  );
+// app/routes/proxy/shop.jsx
+
+import { json } from '@remix-run/node';
+
+export const loader = async ({ request }) => {
+  const shop = request.headers.get("x-shopify-shop-domain"); // "happy-mini.myshopify.com"
+
+  return json({
+    message: `Hello from proxy for ${shop}!`,
+  });
+};
+
+export default function ProxyShopPage() {
+  return <p>This route is for proxy responses only.</p>;
 }
